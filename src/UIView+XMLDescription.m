5d4e33cbd5cafb9af305e2861b49eb06d8bc1b74
Zach Holt <z@wzph.com>
2010-05-01 18:42:22 -0700

    More return types supported, including some structs.

--- src/UIView+XMLDescription.m
+++ src/UIView+XMLDescription.m
@@ -16,55 +16,99 @@
 
 @implementation UIView (XMLDescription)
 
+// If you want to override something in
+// a UIView subclass category, override this,
+// and return an array composed of [UIView gettersForXML]
+// and whatever custom getters you want to add.
 +(NSArray *)gettersForXML {
 	return [NSArray arrayWithObjects:
-	@"tag",
-	@"frame",
-	@"bounds",
-	@"center",
-	@"transform",
-	@"subviews",
-	@"autoresizesSubviews",
-	@"autoresizingMask",
-	@"contentMode",
-	@"contentStretch",
-	@"clipsToBounds",
-	@"backgroundColor",
-	@"alpha",
-	@"isOpaque",
-	@"clearsContextBeforeDrawing",
-	@"isHidden",
-	@"isMultipleTouchEnabled",
-	@"isExclusiveTouch",
+			@"alpha",
+			@"autoresizingMask",
+			@"autoresizesSubviews",
+			@"bounds",
+			@"backgroundColor",
+			@"center",
+			@"clearsContextBeforeDrawing",
+			@"clipsToBounds",
+			@"contentMode",
+			@"contentStretch",
+			@"currentTitle",
+			@"frame",
+			@"isExclusiveTouch",
+			@"isHidden",
+			@"isKeyWindow",
+			@"isMultipleTouchEnabled",
+			@"isOpaque",
+			@"isOn",
+			@"subviews",
+			@"tag",
+			@"text",
+			@"title",
+			@"transform",
 	nil];
+}
+
+- (NSString *) xmlDescriptionWithStringPadding:(NSString *)padding {
+	NSMutableString *resultingXML = [NSMutableString stringWithFormat:@"\n%@<%s>", padding, NAMEOF(self)];
+	[resultingXML appendString:[self xmlAttributesWithPadding:padding]];
+	[resultingXML appendFormat:@"\n%@</%s>", padding, NAMEOF(self)];
+	return resultingXML;
 }
 
 - (NSMutableString *) xmlAttributesWithPadding:(NSString *)padding {
 	NSMutableString *attributes = [NSMutableString stringWithFormat:@"\n%@\t<address>%lu</address>", padding, (NSUInteger)self];
 
 	for ( NSString *getterName in [[self class] gettersForXML] ) {
-		Method theMethod = class_getInstanceMethod( [self class], NSSelectorFromString( getterName ) );
+		SEL getterSelector = NSSelectorFromString( getterName );
+		if ( ![self respondsToSelector:getterSelector] ) { continue; }
+
+		
+		Method theMethod = class_getInstanceMethod( [self class], getterSelector );
 		char *returnType = method_copyReturnType( theMethod );
 
 		NSString *formatString = nil;
 		if ( returnType ) {
 			switch ( returnType[0] ) {
+				case '@':
+					[attributes appendFormat:@"\n%@\t<%@>", padding, getterName];
+
+					id theValue = [self performSelector:getterSelector];
+					if ( [theValue isKindOfClass:[NSArray class]] ) {
+						for ( id thing in (NSArray *)theValue ) {
+							if ( [thing respondsToSelector:@selector( xmlDescriptionWithStringPadding: )] ) {
+								[attributes appendString:[thing xmlDescriptionWithStringPadding:[NSString stringWithFormat:@"%@\t\t", padding]]];
+							}
+							else {
+								[attributes appendFormat:@"\n%@\t\t<%@><![CDATA[%@]]></%@>", padding, NAMEOF( thing ), [thing description], NAMEOF( thing )];
+							}
+						}
+					}
+					else {
+						// Default to an id's description
+						[attributes appendFormat:@"\n%@\t\t<![CDATA[%@]]>", padding, [theValue description]];
+					}
+					[attributes appendFormat:@"\n%@\t</%@>", padding, getterName];
+					break;
 				case 'i':
 				case 'c':
-					formatString = [NSString stringWithFormat:@"%d", [self performSelector:NSSelectorFromString( getterName )]];
+					formatString = [NSString stringWithFormat:@"%d", [self performSelector:getterSelector]];
+					break;
+				case 'I':
+					formatString = [NSString stringWithFormat:@"%u", [self performSelector:getterSelector]];
 					break;
 				case 'd': {
-					continue;
-					double theDouble;
-					objc_msgSend_fpret( (void*)&theDouble, (id)self, NSSelectorFromString( getterName ) );
+					double (*getter)(id, SEL);
+					getter = (double (*)(id, SEL))[self methodForSelector:getterSelector];
+					double theDouble = getter( self, getterSelector );
+					
 					formatString = [NSString stringWithFormat:@"%.6f", theDouble];
 					break;
 				}
 				case 'f': {
-					continue;
-					float theFloat;
-					NSLog( @"About to hit %@", getterName );
-					objc_msgSend_fpret( (void*)&theFloat, (id)self, NSSelectorFromString( getterName ) );
+					float (*getter)(id, SEL);
+					getter = (float (*)(id, SEL))[self methodForSelector:getterSelector];
+					float theFloat = getter( self, getterSelector );
+					
 					formatString = [NSString stringWithFormat:@"%.6f", theFloat];
 					break;
 				}
@@ -72,32 +116,33 @@
 					[attributes appendFormat:@"\n%@\t<%@>", padding, getterName];
 					if ( strnstr( returnType , "{CGRect", 7 ) ) {
 						CGRect (*getter)(id, SEL);
-						
-						getter = (CGRect (*)(id, SEL))[self methodForSelector:NSSelectorFromString( getterName )];
-						CGRect theRect = getter( self, NSSelectorFromString( getterName ) );
-// //#if TARGET_IPHONE_SIMULATOR
-////						CGRect theRect = (CGRect)objc_msgSend( (id)self, NSSelectorFromString( getterName ) );
-////#else
-//			
-//						CGRect theRect = (*(CGRect(*)(void*, void*, ...)) objc_msgSend_stret)( (void*)&theRect, (id)self, NSSelectorFromString( getterName ) );
-////#endif
-////						structInvocation = [NSInvocation invocationWithMethodSignature:[self methodSignatureForSelector:NSSelectorFromString( getterName )]];
-////						[structInvocation setSelector:NSSelectorFromString( getterName )];
-////						[structInvocation invokeWithTarget:self];
-////
-////						
-////						
-////						NSUInteger length = [[structInvocation methodSignature] methodReturnLength];
-////						void *buffer = (void *)malloc(length);
-////						[structInvocation getReturnValue:buffer];
-////						CGRect theRect = (CGRect)buffer;
-////
-					
-						
+						getter = (CGRect (*)(id, SEL))[self methodForSelector:getterSelector];
+						CGRect theRect = getter( self, getterSelector );
+
 						[attributes appendFormat:@"\n%@\t\t<x>%.6f</x>", padding, theRect.origin.x];
 						[attributes appendFormat:@"\n%@\t\t<y>%.6f</y>", padding, theRect.origin.y];
 						[attributes appendFormat:@"\n%@\t\t<width>%.6f</width>", padding, theRect.size.width];
 						[attributes appendFormat:@"\n%@\t\t<height>%.6f</height>", padding, theRect.size.height];
+					}
+					else if ( strnstr( returnType , "{CGPoint", 8 ) ) {
+						CGPoint (*getter)(id, SEL);
+						getter = (CGPoint (*)(id, SEL))[self methodForSelector:getterSelector];
+						CGPoint thePoint = getter( self, getterSelector );
+						
+						[attributes appendFormat:@"\n%@\t\t<x>%.6f</x>", padding, thePoint.x];
+						[attributes appendFormat:@"\n%@\t\t<y>%.6f</y>", padding, thePoint.y];
+					}
+					else if ( strnstr( returnType , "{CGAffineTransform", 18 ) ) {
+						CGAffineTransform (*getter)(id, SEL);
+						getter = (CGAffineTransform (*)(id, SEL))[self methodForSelector:getterSelector];
+						CGAffineTransform theTransform = getter( self, getterSelector );
+						
+						[attributes appendFormat:@"\n%@\t\t<a>%.6f</b>", padding, theTransform.a];
+						[attributes appendFormat:@"\n%@\t\t<b>%.6f</b>", padding, theTransform.b];
+						[attributes appendFormat:@"\n%@\t\t<c>%.6f</c>", padding, theTransform.c];
+						[attributes appendFormat:@"\n%@\t\t<d>%.6f</d>", padding, theTransform.d];
+						[attributes appendFormat:@"\n%@\t\t<tx>%.6f</tx>", padding, theTransform.tx];
+						[attributes appendFormat:@"\n%@\t\t<ty>%.6f</ty>", padding, theTransform.ty];
 					}
 					else {
 						[attributes appendFormat:@"\n%@\t\t<error>Unknown struct : %s</error>", padding, returnType];
@@ -108,11 +153,6 @@
 				default:
 					[attributes appendFormat:@"\n%@\t<%@ error=\"Unknown return type : %s\"/>", padding, getterName, returnType];
 					break;
-			}
-
-			if ( [getterName isEqualToString:@"alpha"] ) {
-				CGFloat theAlpha = [self alpha];
-				NSLog( @"theAlpha = %.6f", theAlpha );
 			}
 
 			if ( formatString ) {
@@ -133,71 +173,14 @@
 	return attributes;
 }
 
-- (NSString *) xmlDescriptionWithStringPadding:(NSString *)padding {
-	NSMutableString *resultingXML = [NSMutableString stringWithFormat:@"\n%@<%s>", padding, NAMEOF(self)];
-	[resultingXML appendString:[self xmlAttributesWithPadding:padding]];
-	
-	//TODO: delegate those to subclasses
-	if ([self respondsToSelector:@selector(text)])
-	{
-		[resultingXML appendFormat:@"\n%@\t<text><![CDATA[%@]]></text>", padding, [self performSelector:@selector(text)]];
-	}
-	if ([self respondsToSelector:@selector(title)])
-	{
-		[resultingXML appendFormat:@"\n%@\t<title><![CDATA[%@]]></title>", padding, [self performSelector:@selector(title)]];
-	}
-	if ([self respondsToSelector:@selector(currentTitle)])
-	{
-		[resultingXML appendFormat:@"\n%@\t<currentTitle><![CDATA[%@]]></currentTitle>", padding, [self performSelector:@selector(currentTitle)]];
-	}
-	if ([self respondsToSelector:@selector(isKeyWindow)])
-	{
-		if([self performSelector:@selector(isKeyWindow)]) {
-			[resultingXML appendFormat:@"\n%@\t<keyWindow>YES</keyWindow>", padding];			
-		}
-		else {
-			[resultingXML appendFormat:@"\n%@\t<keyWindow>NO</keyWindow>", padding];			
-		}
-	}
-	if ([self respondsToSelector:@selector(isOn)])
-	{
-		if(((UISwitch *)self).on) {
-			[resultingXML appendFormat:@"\n%@\t<on>YES</on>", padding];			
-		}
-		else {
-			[resultingXML appendFormat:@"\n%@\t<on>NO</on>", padding];
-		}
-	}
-	
-//	[resultingXML appendFormat:@"\n%@\t<frame>", padding];
-//	[resultingXML appendFormat:@"\n%@\t\t<x>%f</x>", padding, self.frame.origin.x];
-//	[resultingXML appendFormat:@"\n%@\t\t<y>%f</y>", padding, self.frame.origin.y];
-//	[resultingXML appendFormat:@"\n%@\t\t<width>%f</width>", padding, self.frame.size.width];
-//	[resultingXML appendFormat:@"\n%@\t\t<height>%f</height>", padding, self.frame.size.height];
-//	[resultingXML appendFormat:@"\n%@\t</frame>", padding];
-	if(self.subviews.count > 0) {
-		[resultingXML appendFormat:@"\n%@\t<subviews>", padding];
-		for (UIView *subview in [self subviews]) {
-			[resultingXML appendString:[subview xmlDescriptionWithStringPadding:[NSString stringWithFormat:@"%@\t\t", padding]]];
-		}
-		[resultingXML appendFormat:@"\n%@\t</subviews>", padding];
-	}
-	else {
-		[resultingXML appendFormat:@"\n%@\t<subviews />", padding];
-	}
-	[resultingXML appendFormat:@"\n%@</%s>", padding, NAMEOF(self)];
-	return resultingXML;
-}
-
-
 @end
 
 @implementation UITableViewCell (XMLDescription)
 
-- (NSMutableString *) xmlAttributesWithPadding:(NSString *) padding {
-	NSMutableString *attributes = [super xmlAttributesWithPadding:padding];
-	[attributes appendFormat:@"\n%@\t<accessoryType>%d</accessoryType>", padding, [self accessoryType]];
-	return attributes;
++(NSArray *)gettersForXML {
+	NSMutableArray *getters = [NSMutableArray arrayWithArray:[UIView gettersForXML]];
+	[getters addObject:@"accessoryType"];
+	return getters;
 }
 
 @end
